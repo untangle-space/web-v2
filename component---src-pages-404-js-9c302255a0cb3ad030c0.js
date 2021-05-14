175a0c7cb632a39c635898fa114156cd71a42043
untangle-space <untangle-space@users.noreply.github.com>
2021-05-14 04:10:22 +0000

    deployed via Gatsby Publish Action 🎩 for 75eb61cf74b35cb5999293f69f4c11a91b77675d

---
+++ component---src-pages-404-js-9c302255a0cb3ad030c0.js
@@ -0,0 +1,2 @@
+(self.webpackChunkuntangle=self.webpackChunkuntangle||[]).push([[883],{9616:function(t,e,n){"use strict";n.r(e);var a=n(7294),l=n(7198),u=n(6179);e.default=function(t){var e=t.data,n=t.location,o=e.site.siteMetadata.title;return a.createElement(l.Z,{location:n,title:o},a.createElement(u.Z,{title:"404: Not Found"}),a.createElement("h1",null,"Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9c302255a0cb3ad030c0.js.map
